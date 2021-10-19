8d0cf7dcb98fbf357918637a754a1cf56604be9a
Muhammed Aslam <aslam91501@gmail.com>
2021-10-19 17:28:14 +0530

    created roomTypeResponse file

--- src/main/java/com/hotelbooking/api/controller/RoomController.java
+++ src/main/java/com/hotelbooking/api/controller/RoomController.java
@@ -1,5 +1,12 @@
 package com.hotelbooking.api.controller;
 
+import java.util.List;
+
+import com.hotelbooking.api.entity.RoomType;
+import com.hotelbooking.api.entity.dto.RoomTypeResponse;
+import com.hotelbooking.api.service.RoomTypeService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,5 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/room")
 @CrossOrigin(origins = "http://localhost:4200")
 public class RoomController {
-    
+    @Autowired
+    private RoomTypeService roomTypeService;
+
+    // Room type
+    // public List<RoomTypeResponse> getRoomTypes(){
+        
+    // }
 }
---
+++ src/main/java/com/hotelbooking/api/entity/dto/RoomTypeResponse.java
@@ -0,0 +1,5 @@
+package com.hotelbooking.api.entity.dto;
+
+public class RoomTypeResponse {
+
+}
